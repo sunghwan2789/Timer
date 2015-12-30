fb814965a3e8a8684b94b36af7de8248c90a66ce
Sunghwan Bang <sunghwan2789@gmail.com>
2015-12-30 22:01:39 +0900

    Initial commit

---
+++ Timer/History.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Timer
+{
+    /// <summary>
+    /// Interaction logic for History.xaml
+    /// </summary>
+    public partial class History : UserControl
+    {
+        public History()
+        {
+            InitializeComponent();
+        }
+
+        private List<KeyValuePair<int, DateTime>> DB = new List<KeyValuePair<int, DateTime>>();
+
+        public KeyValuePair<int, DateTime> Last = new KeyValuePair<int, DateTime>(0, DateTime.Now);
+
+        public void Insert(int time)
+        {
+            Last = new KeyValuePair<int, DateTime>(time, DateTime.Now);
+            DB.RemoveAll(i => i.Key == Last.Key);
+            DB.Add(Last);
+
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            using (var d = Dispatcher.DisableProcessing())
+            {
+                container.Children.Clear();
+
+                foreach (var rec in DB.Reverse<KeyValuePair<int, DateTime>>())
+                {
+                    var wrapper = new Grid();
+                    wrapper.Tag = rec;
+                    wrapper.ColumnDefinitions.Add(new ColumnDefinition());
+                    wrapper.ColumnDefinitions.Add(new ColumnDefinition());
+                    wrapper.ColumnDefinitions[1].Width = new GridLength(20);
+                    wrapper.Margin = new Thickness(0, 7, 0, 7);
+
+                    var textBlock = new TextBlock();
+                    textBlock.Text = string.Format("{0:00}:{1:00}", rec.Key / 60, rec.Key % 60);
+                    textBlock.PreviewMouseDown += Time_Click;
+                    textBlock.FontSize = 14;
+                    textBlock.Margin = new Thickness(7, 0, 7, 0);
+                    Grid.SetColumn(textBlock, 0);
+                    wrapper.Children.Add(textBlock);
+
+                    var closeBtn = new TextBlock();
+                    closeBtn.Text = "🗙";
+                    closeBtn.PreviewMouseDown += Close_Click;
+                    Grid.SetColumn(closeBtn, 1);
+                    wrapper.Children.Add(closeBtn);
+
+                    container.Children.Add(wrapper);
+                }
+            }
+        }
+
+        public Action<int> RecordClicked;
+
+        private void Time_Click(object sender, RoutedEventArgs e)
+        {
+            var textBlock = (TextBlock) sender;
+            var wrapper = (Grid) textBlock.Parent;
+            var rec = (KeyValuePair<int, DateTime>) wrapper.Tag;
+            if (RecordClicked != null)
+            {
+                RecordClicked(rec.Key);
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            var closeBtn = (TextBlock) sender;
+            var wrapper = (Grid) closeBtn.Parent;
+            var rec = (KeyValuePair<int, DateTime>) wrapper.Tag;
+            DB.RemoveAll(i => i.Key == rec.Key);
+
+            UpdateUI();
+        }
+    }
+}
---
+++ Timer/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            HistoryPanel.RecordClicked = LoadTime;
+        }
+
+        private void DrawTime(int leftTime)
+        {
+            Timer.Text = string.Format("{0:00}:{1:00}", leftTime / 60, leftTime % 60);
+        }
+
+        private SoundPlayer Bell = new SoundPlayer(Assembly.GetExecutingAssembly().GetManifestResourceStream("Timer.Bell.wav"));
+
+        private CancellationTokenSource TimerThreadCTS = new CancellationTokenSource();
+        private async void TimerThread()
+        {
+            int leftTime;
+            do
+            {
+                await Task.Delay(200);
+
+                var ellapsed = DateTime.Now.Subtract(HistoryPanel.Last.Value).TotalSeconds;
+                leftTime = HistoryPanel.Last.Key - (int) ellapsed;
+
+                if (TimerThreadCTS.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                Dispatcher.Invoke(() => DrawTime(leftTime));
+            } while (leftTime > 0);
+
+            // 알람
+            Bell.PlayLooping();
+        }
+
+        /// <summary>
+        /// <see cref="Start"/>를 클릭하면 호출되는 함수.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="Timer"/>의 Text를 초 단위로 변환해 <see cref="SetTime"/>의 값으로 지정하고,
+        /// <see cref="TimerThread"/>를 Run한다.
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Start.Visibility = Visibility.Hidden;
+
+            TimerThreadCTS.Cancel();
+
+            int time = 0;
+            var timeArray = Timer.Text.Split(':');
+            // dd:?HH:mm:ss
+            if (timeArray.Length > 3)
+            {
+                throw new OverflowException("TimeValue Overflow!");
+            }
+            // HH:?mm:ss
+            if (timeArray.Length >= 2)
+            {
+                foreach (var part in timeArray)
+                {
+                    time = time * 60 + int.Parse(string.IsNullOrEmpty(part) ? "0" : part);
+                }
+            }
+            // ss?
+            else
+            {
+                time = int.Parse(timeArray.LastOrDefault() ?? "0");
+            }
+
+            HistoryPanel.Insert(time);
+            TimerThreadCTS = new CancellationTokenSource();
+            Task.Run((Action) TimerThread, TimerThreadCTS.Token);
+
+            Pause.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// <see cref="Pause"/>를 클릭하면 호출되는 함수.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="TimerThread"/>를 Abort한다.
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            Pause.Visibility = Visibility.Hidden;
+
+            TimerThreadCTS.Cancel();
+
+            Bell.Stop();
+
+            Start.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// <see cref="Reset"/>을 클릭하면 호출되는 함수.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="Pause_Click"/>을 호출하고,
+        /// <see cref="Timer"/>의 Text를 <see cref="History.Last"/>의 Key로 바꾼다.
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Pause_Click(sender, e);
+
+            DrawTime(HistoryPanel.Last.Key);
+        }
+
+        /// <summary>
+        /// <see cref="HistoryPanel"/>의 항목을 클릭하면 호출되는 함수.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="Pause_Click"/>을 호출하고,
+        /// <see cref="Timer"/>의 Text를 <paramref name="time"/>으로 바꾼다.
+        /// </remarks>
+        /// <param name="time"></param>
+        private void LoadTime(int time)
+        {
+            Pause_Click(null, null);
+
+            DrawTime(time);
+        }
+
+        private void Timer_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ":")
+            {
+                return;
+            }
+
+            e.Handled = true;
+
+            int res;
+            if (!int.TryParse(e.Text, out res))
+            {
+                return;
+            }
+
+            var start = Timer.SelectionStart;
+            // 숫자 삽입 모드
+            if (start >= Timer.Text.Length)
+            {
+                Timer.Text += e.Text;
+            }
+            // 숫자 수정 모드
+            else
+            {
+                var end = start + 1;
+                if (Timer.Text[start] == ':')
+                {
+                    var prevSplitterIdx = Timer.Text.Substring(0, start).LastIndexOf(':');
+                    // 부분 시간이 2글자 이상이면 : 뛰어넘기
+                    if (start - prevSplitterIdx - 1 >= 2)
+                    {
+                        start++;
+                        end++;
+                    }
+                    // 미만이면 숫자 채우기
+                    else
+                    {
+                        end = start;
+                    }
+                }
+                Timer.Text = Timer.Text.Substring(0, start) + e.Text + Timer.Text.Substring(end);
+            }
+            Timer.SelectionStart = start + 1;
+        }
+    }
+}
